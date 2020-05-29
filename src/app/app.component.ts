91a0580c3ffc2947033665977922553c6b6c4d9d
Marco Molteni <moltenma@gmail.com>
2020-05-29 12:26:00 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-menu';
+
+  // we create an object that contains coordinates
+  menuTopLeftPosition =  {x: '0', y: '0'}
+
+  // reference to the MatMenuTrigger in the DOM
+  @ViewChild(MatMenuTrigger, {static: true}) matMenuTrigger: MatMenuTrigger;
+
+  /**
+   * Method called when the user click with the right button
+   * @param event MouseEvent, it contains the coordinates
+   * @param item Our data contained in the row of the table
+   */
+  onRightClick(event: MouseEvent, item) {
+      // preventDefault avoids to show the visualization of the right-click menu of the browser
+      event.preventDefault();
+
+      // we record the mouse position in our object
+      this.menuTopLeftPosition.x = event.clientX + 'px';
+      this.menuTopLeftPosition.y = event.clientY + 'px';
+
+      // we open the menu
+      // we pass to the menu the information about our object
+      this.matMenuTrigger.menuData = {item: item}
+
+      // we open the menu
+      this.matMenuTrigger.openMenu();
+
+  }
+
+  // number of lines to show for the example
+  getExamples(n: number) {
+    return [... Array(n).keys()];
+  }
 }
